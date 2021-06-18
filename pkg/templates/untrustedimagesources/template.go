13d2dbe1d34fc4b75e1a3ff2b5bc21b41dc50009
Priyanka Saggu <priyankasaggu11929@gmail.com>
2021-06-18 12:32:47 +0530

    [wip] add untrustedimagesources check

---
+++ pkg/templates/untrustedimagesources/template.go
@@ -0,0 +1,48 @@
+package untrustedimagesurces
+
+import (
+	"fmt"
+	"strings"
+
+	"golang.stackrox.io/kube-linter/pkg/check"
+	"golang.stackrox.io/kube-linter/pkg/config"
+	"golang.stackrox.io/kube-linter/pkg/diagnostic"
+	"golang.stackrox.io/kube-linter/pkg/objectkinds"
+	"golang.stackrox.io/kube-linter/pkg/templates"
+	"golang.stackrox.io/kube-linter/pkg/templates/util"
+	v1 "k8s.io/api/core/v1"
+	"golang.stackrox.io/kube-linter/pkg/templates/untrustedimagesources/internal/params"
+)
+
+const (
+	templateKey = "untrusted-image-sources"
+)
+
+func init() {
+	templates.Register(check.Template{
+		HumanName:   "Untrusted image sources",
+		Key:         templateKey,
+		Description: "Flag applications running containers with untrusted container image source",
+		SupportedObjectKinds: config.ObjectKindsDesc{
+			ObjectKinds: []string{objectkinds.DeploymentLike},
+		},
+		Parameters:             params.ParamDescs,
+		ParseAndValidateParams: params.ParseAndValidate,
+		Instantiate: params.WrapInstantiateFunc(func(p params.Params) (check.Func, error) {
+			return util.PerContainerCheck(func(container *v1.Container) []diagnostic.Diagnostic {
+
+				// checking whether the container image comes from a trusted image source or not
+
+				res := true
+
+				for _, imageSource := range p.AllowedImageSources {
+					res = strings.Contains(container.Image, imageSource)
+				}
+				if ! res {
+					return []diagnostic.Diagnostic{{Message: fmt.Sprintf("container %q image doesn't have a valid image source", container.Name)}}
+				}
+				return nil
+			}), nil
+		}),
+	})
+}
